ad42f5590cc2f3a4d154fa275a59e46fb0de1612
navidabdi <artsanaat@gmail.com>
2022-03-29 09:59:59 +0430

    make an index file in the images folder to export all of the images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { iconPaper } from './images';
 function App() {
   return (
     <div className="App">
       <div>Hello</div>
+      <div>
+        <img src={iconPaper} alt="" />
+      </div>
     </div>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,25 @@
+import bgPentagon from './bg-pentagon.svg';
+import bgTriangle from './bg-triangle.svg';
+import iconClose from './icon-close.svg';
+import iconLizard from './icon-lizard.svg';
+import iconPaper from './icon-paper.svg';
+import iconRock from './icon-rock.svg';
+import iconScissors from './icon-scissors.svg';
+import imageRulesBonus from './image-rules-bonus.svg';
+import imageRules from './image-rules.svg';
+import logoBonus from './logo-bonus.svg';
+import logo from './logo.svg';
+
+export {
+  bgPentagon,
+  bgTriangle,
+  iconClose,
+  iconLizard,
+  iconPaper,
+  iconRock,
+  iconScissors,
+  imageRulesBonus,
+  imageRules,
+  logoBonus,
+  logo,
+};
